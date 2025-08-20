3a4f60a77c68fd4eed7172fbf76eb3002642d7c2
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-08-20 16:28:35 +0300

    Update Constants.cs

--- Zune.Net.Shared/Constants.cs
+++ Zune.Net.Shared/Constants.cs
@@ -5,5 +5,6 @@
         public const string USERAGENT_ZUNE48 = "Zune/4.8";
         public const string USERAGENT_ZUNE48_GITHUB = "Zune/4.8 +https://github.com/ZuneDev/ZuneNetApi";
         public const string USERAGENT_UIX = "UIX/2008";
+        public const string FM_API_KEY = "f4bfc85382e2f939f7daea1b23d5702d";
     }
 }
