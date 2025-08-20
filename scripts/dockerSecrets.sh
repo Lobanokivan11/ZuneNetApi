4d4f0bb2094f327ce73717c927fdd39ef329acb8
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-08-20 16:56:02 +0300

    Update dockerSecrets.sh

--- scripts/dockerSecrets.sh
+++ scripts/dockerSecrets.sh
@@ -4,7 +4,7 @@ export DOMAIN_ROOT=zune.net
 export SSL_ACME_EMAIL=test@example.com
 
 export MONGO_INITDB_ROOT_USERNAME=root
-export MONGO_INITDB_ROOT_PASSWORD=ChangeMe
+export MONGO_INITDB_ROOT_PASSWORD=root
 
 export ZuneNetContext__ConnectionString=mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@mongodb:27017/?authSource=admin
 export ZuneNetContext__DatabaseName=Zune
