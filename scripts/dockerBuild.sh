ec838672d035988caff9d74bf9891d7cd89f5772
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-08-20 17:17:31 +0300

    Update dockerBuild.sh

--- scripts/dockerBuild.sh
+++ scripts/dockerBuild.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# Get Docker
-curl -fsSL https://get.docker.com/rootless | sh
-export $HOME/bin:$PATH
-export DOCKER_HOST=unix:///run/user/1000/docker.sock
-
 # Import secrets as environment variables
 SCRIPT_DIR=$( dirname -- "$( readlink -f -- "$0"; )"; )
 . $SCRIPT_DIR/dockerSecrets.sh
